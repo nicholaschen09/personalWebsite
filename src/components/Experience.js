72125eab3c65f1f7b1ddc5f10983fcedd6717e74
nicholaschen09 <nicholas.chen243@gmail.com>
2025-02-16 16:05:41 -0500

    clean up v1

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './Experience.css';
-import RBCxLogo from './RBCx.png';
 import OwnrLogo from './ownr.jpeg';
-import UWAFTLogo from './uwaft.png';
 import RBCLogo from './images.png';
 import MetaHashCapitalLogo from './metahashcapital.jpeg';
-import VolunTrackLogo from './voluntrack.webp';
 
 const Experience = () => {
     const handleGoBack = () => {
@@ -21,14 +18,7 @@ const Experience = () => {
             description: "Ownr is an RBC Ventures company associated with RBCx",
             logo: OwnrLogo,
         },
-        {
-            role: "Electrical Engineer",
-            company: "University of Waterloo EcoCAR Team (UWAFT) (On-Call)",
-            duration: "Sep 2024 – Present (5 mos)",
-            location: "Waterloo, Ontario, Canada (Hybrid)",
-            description: "Helping build an alternative fuel car that is eco-friendly and efficient",
-            logo: UWAFTLogo,
-        },
+
         {
             role: "Innovation Developer Intern",
             company: "RBC (Internship)",
@@ -46,15 +36,6 @@ const Experience = () => {
             description: "Designed wireframes and prototypes using Figma for the company’s product to allow for better user experience",
             skills: "Design, Figma, User Experience (UX)",
             logo: MetaHashCapitalLogo,
-        },
-        {
-            role: "UX/UI Design Intern",
-            company: "VolunTrack Org. (On-Call)",
-            duration: "Mar 2022 – Oct 2022 (8 mos)",
-            location: "Richmond Hill, Ontario, Canada (Hybrid)",
-            description: "Developed and designed intuitive user interfaces and screens for the mobile application",
-            skills: "Figma",
-            logo: VolunTrackLogo,
         },
     ];
 
